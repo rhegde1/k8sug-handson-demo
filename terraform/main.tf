aa9e3068af129acf0d47e87195ae274d45e3511f
Ritesh Hegde <103172854+rh-sph@users.noreply.github.com>
2023-09-14 18:25:02 +0800

    update instruction.sh

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,15 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "handsonaksdemo"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "handsonaksdemo"
-  # http_application_routing_enabled = true
+  name                             = "handsonaksdemo"
+  location                         = azurerm_resource_group.rg.location
+  resource_group_name              = azurerm_resource_group.rg.name
+  dns_prefix                       = "handsonaksdemo"
+  #http_application_routing_enabled = true
 
   default_node_pool {
     name       = "default"
-    node_count = "1"
+    node_count = "2"
     vm_size    = "standard_d2_v2"
   }
 
